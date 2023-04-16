08a0fcc61c1377d9af680f76bcba99d74cbe6eec
aridguy <rotimiariyo@gmail.com>
2023-04-16 15:16:52 +0100

    updated file

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -4,10 +4,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <div className="form-outline">
-                <input type="text" id="form12" className="form-control" />
-                <label className="form-label">Example label</label>
-            </div>
+           Home
         </div>
     )
 }
