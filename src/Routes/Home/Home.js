57924046c80e7dc7e7cb63aa94c1930891922129
aridguy <rotimiariyo@gmail.com>
2023-04-16 18:52:31 +0100

    modified the home page

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css'
+import LandImage from '../../Assets/ambassadors_eafahi.png.svg'
 
 const Home = () => {
     return (
@@ -10,7 +11,7 @@ const Home = () => {
                     <div className="col-md-8 mt-5">
                         <div className="contentsHeader text-center">
                             <p className="heading1">
-                                Trade <span>gift cards & crypto</span> <br /> with ease
+                                <span className="very-black">Trade</span> <span>gift cards & crypto</span> <br /> <span className="very-black">with ease</span>
                             </p>
                             <div className="row">
                                 <div className="col-md-3"></div>
@@ -19,9 +20,19 @@ const Home = () => {
                                         Enjoy a seamless and hassle free experience, trading your gift cards and cryptocurrencies with
                                         The Shawn Exchange.
                                     </span>
+                                    <button className="getStartedBtn mt-3">Get Started</button>
                                 </div>
                                 <div className="col-md-3"></div>
                             </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-2"></div>
+                            <div className="col-md-8">
+                                <div className="potoLanding text-center">
+                                    <img src={LandImage} alt="lading page icon" />
+                                </div>
+                            </div>
+                            <div className="col-md-2"></div>
                         </div>
                     </div>
                     <div className="col-md-2"></div>
