2969201dba7b97182fa831fb7b3ac60f6cf2a0a7
aridguy <rotimiariyo@gmail.com>
2023-04-16 14:28:41 +0100

    second commit

---
+++ src/Routes/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className="form-outline">
+                <input type="text" id="form12" className="form-control" />
+                <label className="form-label">Example label</label>
+            </div>
+        </div>
+    )
+}
+
+export default Home
